ea2207970890dae4654f10be65bdc9a0476afa71
CharaRosa <loelondjerose@gmail.com>
2024-09-02 13:10:50 +0100

    first commit

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    product_object = Product.objects.all()
+    return render(request, 'shop/index.html', {'product_object': product_object})
